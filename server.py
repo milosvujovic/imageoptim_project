fc72bda8f7f5df7a94c7c53e6c3a0ba5c79bf28e
Owain Lansdowne <LansdowneOP@cardiff.ac.uk>
2021-03-18 10:54:47 +0000

    Connects to the database now

--- server.py
+++ server.py
@@ -1,18 +1,36 @@
 # Imports
 import os
 import json
-from flask import Flask, redirect, request, render_template, jsonify, make_response, url_for, abort
+from flask import Flask, render_template, request
+from flask_mysqldb import MySQL
 
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # Variables
 app = Flask(__name__)
+# Connects to the database
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'comsc'
+app.config['MYSQL_DB'] = 'imageoptim'
+
+mysql = MySQL(app)
+
+
 
 #Webpage Routes
 @app.route("/")
 def defaultPage():
+    attemptToReadFromDatabase()
     return "Default Page"
 
+
+# Example connection to the database
+def attemptToReadFromDatabase():
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO tier(idTier, type) VALUES (%s, %s)", (4, "global company"))
+        mysql.connection.commit()
+        cur.close()
 
 
 
