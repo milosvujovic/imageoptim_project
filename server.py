50b496f67dbdbfaf38ec24f8906b1e9b6018f182
Owain Lansdowne <LansdowneOP@cardiff.ac.uk>
2021-03-26 10:07:25 +0000

    Can now purchase multiple licences

--- server.py
+++ server.py
@@ -23,26 +23,15 @@ app.config['MAIL_USERNAME'] = 'group11IMAGEOPTIM@outlook.com'
 app.config['MAIL_PASSWORD'] = '1m@g30ptim'
 app.config["MAIL_USE_SSL:1123"] = True
 app.config["MAIL_USE_TLS"] = True
-# app.config['SESSION_PERMANENT'] = True
-# app.config['SESSION_COOKIE_PATH'] = '/'
-# app.permanent_session_lifetime = datetime.timedelta(days=365)
 app.secret_key = 'fj590Rt?h40gg'
 
 mail = Mail(app)
 mysql = MySQL(app)
 
-# @app.before_first_request
-# def begin():
-#     print("Beginnning the system")
-#     session['basket'] = {}
-#     print(session['basket'])
 
 #Webpage Routes
 @app.route("/")
 def homePage():
-    if 'basket' in session:
-        print("home")
-        print(session['basket'])
     return render_template('home.html', title = "Home", licences = readFromDatabaseUsingStoredProcedures("getListOfLicence()"))
 
 # Displays checkout page asking for users details.
@@ -56,9 +45,6 @@ def customerPage():
 # Displays page with options to select a licence
 @app.route("/licence/<licenceID>")
 def selectLicence(licenceID):
-    if 'basket' in session:
-        print("select licence")
-        print(session['basket'])
     callTiers = "getTiersForLicence("+licenceID+")"
     callLengths = "getLengthOfLicences("+licenceID+")"
     return render_template('licence.html', title = "Licence", tiers = readFromDatabaseUsingStoredProcedures(callTiers), lengths= readFromDatabaseUsingStoredProcedures(callLengths), licenceID = licenceID)
@@ -67,24 +53,29 @@ def selectLicence(licenceID):
 # Having read the details about each item from the database.
 @app.route("/basket")
 def basketPage():
-    if 'basket' in session:
-        print("basket")
-        print(session['basket'])
-            # callItem = "getBasketDetails(" + session.get('basket')[1]['tier'] +","+ session.get('basket')[1]['length'] + ")"
-        return render_template('basket.html', title = "Basket", basket =  [], size = 1)
-    else:
-        return render_template('basket.html', title = "Basket", basket =  [], size = 0)
+        basketDetails = gatherBasketDetails()
+        return render_template('basket.html', title = "Basket", basket =  basketDetails[0], size = basketDetails[2], price = basketDetails[1])
 
 # Displays purchase confirmation page
 def purchaseConfirmationPage():
     return render_template('purchase_confirmation.html', title = "Purchase Confirmation")
 
-@app.route("/remove")
-def removeFromBasket():
-    # Removes everything from the basket and redirects them to the basket
-     session.pop('basket', None)
-     return redirect('/')
+@app.route("/basket/remove/<licenceID>")
+def removeFromBasket(licenceID):
+    # Removes selected item from the basket and redirects them to the basket
+    if 'basket' in session:
+        if licenceID in session['basket']:
+            session['basket'].pop(licenceID, None)
+            session.modified = True
+    return basketPage()
 
+@app.route("/basket/clear")
+def removeAllFromBasket():
+    # Removes selected item from the basket and redirects them to the basket
+    if 'basket' in session:
+        session['basket'].clear()
+        session.modified = True
+    return basketPage()
 
 # Reading forms.
 @app.route("/gatherCustomerData", methods=['POST'])
@@ -101,6 +92,7 @@ def customerForm():
         session['customer']['country'] = request.form['countries']
         session['customer']['postcode'] = request.form['postcode']
         session['customer']['vatNumber'] = request.form['vatNumber']
+        session.modified = True
         # Calls function to process transactions
         return processTransaction()
     return "Error with form"
@@ -109,15 +101,10 @@ def customerForm():
 def licenceForm():
     if request.method == 'POST':
         if 'basket' not in session:
-            print("creating new basket")
             session['basket'] = {}
-        print("before adding")
-        print(session['basket'])
         licenceID = (request.form['licenceID'])
         session['basket'][licenceID] = {'tier' : request.form['tier'], 'length' : request.form['length'] }
         session.modified = True
-        print("After adding")
-        print(session['basket'])
     # Redirects them to the basket
     return basketPage()
 
@@ -134,12 +121,13 @@ def writeToDatabaseWithCustomerDetails(name, street, city, postcode,country,emai
 
 def writePurchaseIntoDatabase(customerID):
     # Writes to the database with details of the purchase
-    cur = mysql.connection.cursor()
-    cur.execute("CALL recordPurchase(%s,%s,%s);", (session.get('tier'), session.get('length'), customerID))
-    mysql.connection.commit()
-    data = cur.fetchall()
-    cur.close()
-    print(data)
+    if 'basket' in session:
+        for item in session['basket'].values():
+            cur = mysql.connection.cursor()
+            cur.execute("CALL recordPurchase(%s,%s,%s);", (item.get('tier'), item.get('length'), customerID))
+            mysql.connection.commit()
+            data = cur.fetchall()
+            cur.close()
 
 def readFromDatabaseUsingStoredProcedures(function):
         command = "CALL " + function +";"
@@ -156,10 +144,10 @@ def readFromDatabaseUsingStoredProcedures(function):
 
 
 # Functions
-def sentCustomerEmail(recipient,name, body):
+def sentCustomerEmail(recipient,name, body, price):
     # Prepares the email with the main body of the email being a html template
     msg = Message(subject='Confirmation Email',sender='group11IMAGEOPTIM@outlook.com', recipients = [recipient])
-    msg.html = render_template('emailConfirmationCustomer.html',basket = body, name = name)
+    msg.html = render_template('emailConfirmationCustomer.html',basket = body, name = name, price = price)
     # Attaches the invoice file
     with app.open_resource('static\invoice\invoice.pdf') as fp:
         msg.attach('invoice.pdf', "invoice/pdf", fp.read())
@@ -169,13 +157,13 @@ def sentCustomerEmail(recipient,name, body):
     # Sends the email
     mail.send(msg)
 
-def sentAdminEmail(recipient,companyName, customerName,emailAddress, body):
+def sentAdminEmail(recipient,companyName, customerName,emailAddress, body, price):
     # Prepares the email with the main body of the email being a html template
     recipients = [ ]
     for i in recipient:
         recipients.append(i)
     msg = Message(subject='Purchase Confirmation',sender='group11IMAGEOPTIM@outlook.com', recipients = recipients)
-    msg.html = render_template('emailConfirmationAdmin.html',basket = body, customer = companyName,employeeName = customerName,emailAddress = emailAddress)
+    msg.html = render_template('emailConfirmationAdmin.html',basket = body, customer = companyName,employeeName = customerName,emailAddress = emailAddress,price = price)
     # Attaches the contract file
     with app.open_resource('static\contract\contract.pdf') as fp:
         msg.attach('contract.pdf', "contract/pdf", fp.read())
@@ -188,20 +176,32 @@ def processTransaction():
     # Writes the details of the purchase into the database
     writePurchaseIntoDatabase(id)
     # Reads the details of the basket from the datbase to put in email
-    callItem = "getBasketDetails(" + session.get('tier') +","+ session.get('length') + ")"
-    basketDetails = readFromDatabaseUsingStoredProcedures(callItem)
+    basketDetails = gatherBasketDetails()
     # Sents the email to the customer with details of their purchase
-    sentCustomerEmail(session.get("customer")["email"], session.get("customer")["nameOfContactPerson"], basketDetails)
+    sentCustomerEmail(session.get("customer")["email"], session.get("customer")["nameOfContactPerson"], basketDetails[0],basketDetails[1])
     #  Reads the email address of the admin from the datbase
     callItem = "getAdminEmail()"
     adminEmails = readFromDatabaseUsingStoredProcedures(callItem)
     # Sents email to the admin with details of the purchase
-    sentAdminEmail(adminEmails[0],session.get("customer")["name"], session.get("customer")["nameOfContactPerson"],session.get("customer")["email"], basketDetails)
+    sentAdminEmail(adminEmails[0],session.get("customer")["name"], session.get("customer")["nameOfContactPerson"],session.get("customer")["email"], basketDetails[0],basketDetails[1])
     # Clears the basket
-    session.pop('tier', None)
-    session.pop('length', None)
+    session['basket'].clear()
     # Redirects to confirmation page.
     return purchaseConfirmationPage()
+
+def gatherBasketDetails():
+    basketArray = []
+    price = 0
+    size = 0
+    if 'basket' in session:
+        for key,item in session['basket'].items():
+            callItem = "getBasketDetails(" +item['tier'] +","+ item['length'] + ")"
+            temp = list(readFromDatabaseUsingStoredProcedures(callItem)[0])
+            temp.append(key)
+            basketArray.append(temp)
+            price = price + temp[4]
+            size =  size + 1
+    return basketArray,price,size
 
 if __name__ == "__main__":
     app.run(debug=True)
